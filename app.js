e2f37275291bf9d0e9c0df594c69c72196fef915
Zachary Berry <zachberry@gmail.com>
2016-06-07 23:06:13 -0400

    Adds services

--- app.js
+++ app.js
@@ -3,8 +3,114 @@ var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
+app.use(express.bodyParser());
+
 app.get('/', function(req, res) {
 	res.sendfile('public/index.html');
+});
+
+var MongoClient = require('mongodb').MongoClient
+var ObjectID = require('mongodb').ObjectID
+var format = require('util').format;
+
+// MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+// 	if(err)
+// 	{
+// 		throw err;
+// 	}
+
+// 	var collection = db.collection('test_insert');
+// 	collection.insert({a:2}, function(err, docs) {
+// 		collection.count(function(err, count) {
+// 			console.log(format("count = %s", count));
+// 		});
+
+// 		collection.find().toArray(function(err, results) {
+// 			console.log(results);
+// 			db.close();
+// 		});
+// 	});
+// });
+
+//api
+// get all stacks
+app.get('/stack', function(req, res) {
+	MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+		if(err)
+		{
+			throw err;
+		}
+
+		var collection = db.collection('stacks');
+		collection.find().toArray(function(err, results) {
+			res.send(results);
+			db.close();
+		});
+	});
+});
+
+// create new stack
+app.post('/stack', function(req, res) {
+	MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+		if(err)
+		{
+			throw err;
+		}
+
+		var collection = db.collection('stacks');
+		collection.insert({ projects:'[]' }, function(err, docs) {
+			res.send(docs[0]._id);
+			db.close();
+		});
+	});
+});
+
+// get stack xyz
+app.get('/stack/:id', function(req, res) {
+	MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+		if(err)
+		{
+			throw err;
+		}
+
+		var collection = db.collection('stacks');
+		console.log(req.params.id);
+		collection.findOne({ _id:ObjectID(req.params.id) }, function(err, doc) {
+			console.log(doc);
+			res.send(doc);
+			db.close();
+		});
+		
+	});
+});
+
+// update stack xyz
+app.put('/stack/:id', function(req, res) {
+	console.log(req.body.projects);
+	MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+		if(err)
+		{
+			throw err;
+		}
+
+		var collection = db.collection('stacks');
+
+		collection.findAndModify(
+			{ _id:ObjectID(req.params.id) },
+			[],
+			{ projects:req.body.projects },
+			{ upsert: false, 'new':true },
+			function(err, doc) {
+				res.send(doc);
+				db.close();
+			}
+		);
+	});
+});
+
+// delete stack xyz
+app.delete('/stack/xyz', function(req, res) {
+
 });
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,47 @@
-function ProjectCtrl($scope) {
-	$scope.projects = [
+var stackdoServices = angular.module('stackdoServices', ['ngResource']);
+
+stackdoServices.factory('List', ['$resource', function($resource) {
+	return $resource('stack/:stackId', {}, {
+		query: {
+			method: 'GET',
+			params: { stackId:'52c8bf22b7e9f451272a7063' },
+			isArray: true,
+			transformResponse: function(data, header) {
+				return angular.fromJson(angular.fromJson(data).projects);
+			}
+		},
+		update: {
+			method: 'PUT'
+		}
+	});
+}]);
+
+
+
+var stackdo = angular.module('stackdo', ['stackdoServices']);
+
+stackdo.controller('ProjectCtrl', ['$scope', 'List', function($scope, List) {
+	/*$scope.projects = [
 		{name:'1', done:false},
 		{name:'2', done:false},
 		{name:'3', done:false}
-	];
+	];*/
+
+	$scope.projects = List.query();
+
+	$scope.action = 'Update';
+
+	$scope.save = function() {
+		List.update({id: '52c8bf22b7e9f451272a7063'}, $scope.projects, function() {
+			console.log('up dooted');
+		});
+	};
 
 	$scope.addProject = function() {
 		$scope.projects.push({name:$scope.projectName, done:false});
 		$scope.projectName = '';
+
+		$scope.save();
 	};
 
 	$scope.completeProject = function(project, $event) {
@@ -31,4 +65,4 @@ function ProjectCtrl($scope) {
 	$scope.deleteProject = function(project) {
 		$scope.projects.splice($scope.projects.indexOf(project), 1);
 	};
-}
+}]);
